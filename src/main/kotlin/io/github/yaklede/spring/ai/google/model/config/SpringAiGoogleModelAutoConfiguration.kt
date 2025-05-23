598495ea687ac3fb75e98a64189789cebf14975c
Yaklede <chh78151@naver.com>
2025-05-24 05:45:29 +0900

    feature: auto configuration 추가

---
+++ src/main/kotlin/io/github/yaklede/spring/ai/google/model/config/SpringAiGoogleModelAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package io.github.yaklede.spring.ai.google.model.config
+
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+
+@AutoConfiguration
+@ComponentScan(basePackages = ["io.github.yaklede.spring.ai.google.model"])
+class SpringAiGoogleModelAutoConfiguration

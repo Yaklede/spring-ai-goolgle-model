38816af3cef497e8bc17ddc0b4ee61930479b6b1
Yaklede <chh78151@naver.com>
2025-05-24 04:30:02 +0900

    featrure: project init

---
+++ src/main/kotlin/io/github/yaklede/spring/ai/google/model/SpringAiGoogleModelApplication.kt
@@ -0,0 +1,11 @@
+package io.github.yaklede.spring.ai.google.model
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiGoogleModelApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiGoogleModelApplication>(*args)
+}
